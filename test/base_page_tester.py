72a553316ba42358910b455a6c6021b0a53188db
Adrian Perez <appperez@ucdavis.edu>
2025-01-27 10:11:52 -0800

    Add test case to test with reformatting

--- test/base_page_tester.py
+++ test/base_page_tester.py
@@ -1,5 +1,6 @@
 # To get the files from lstore
 import sys
+
 sys.path.append('../lstore')
 
 import unittest
@@ -7,16 +8,21 @@ from base_page import BasePage
 
 
 class MyTestCase(unittest.TestCase):
-	def test(self):
-		test_page = BasePage(5)
-		for i in range(5):
-			test_page.new_record([123,123,123,123,123])
-			record = test_page.get_record(i, [1,1,1,1,1])
-			self.assertEqual(record, [123,123,123,123,123])
+  def test_new_record(self):
+    test_page = BasePage(5)
+    for i in range(5):
+      test_page.new_record([123, 123, 123, 123, 123])
+      record = test_page.get_record(i, [1, 1, 1, 1, 1])
+      self.assertEqual(record, [123, 123, 123, 123, 123])
 
-	#def test_write(self):
+  def test_has_capacity(self):
+    test_page = BasePage(1)
+    for i in range(512):
+      self.assertTrue(test_page.has_capacity())
+      test_page.new_record([1])
 
+    self.assertFalse(test_page.has_capacity())
 
 
 if __name__ == '__main__':
-	unittest.main()
+  unittest.main()
