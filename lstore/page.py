aea6e40904a97b1b8159508a9ba88f09bfe34470
Adrian Perez <appperez@ucdavis.edu>
2025-02-01 21:10:55 -0800

    Reformat and modify write so updates can be made to the physical pages

--- lstore/page.py
+++ lstore/page.py
@@ -1,33 +1,33 @@
-
 class Page:
 
-    def __init__(self):
-        self.num_records = 0
-        self.data = bytearray(4096)
+  def __init__(self):
+    self.num_records = 0
+    self.data = bytearray(4096)
 
-    def has_capacity(self):
-        # each page can hold 512 8 bytes records
-        return self.num_records < 512
+  def has_capacity(self):
+    # each page can hold 512 8 bytes records
+    return self.num_records < 512
 
-    def write(self, value, slot):
-        """
-        Writing to the page. Slot number has to be specified. Value is converted into bytes because
-        the page stores data in a byte array.
-        """
-        # Value is a 64 bit (aka 8byte) int
-        if self.has_capacity():
-            val_to_byte = value.to_bytes(8, byteorder='big')
-            # Insert by bytes
-            i = slot * 8
-            for byte in val_to_byte:
-                self.data[i] = byte
-                i += 1
-            self.num_records += 1
-        else:
-            raise IndexError("Physical Page is full")
+  def write(self, value, slot):
+    """
+    Writing to the page. Slot number has to be specified. Value is converted into bytes because
+    the page stores data in a byte array.
+    """
+    # Value is a 64 bit (aka 8byte) int
 
-    def read(self, slot):
-      if slot > 512 or slot < 0 or slot >= self.num_records: raise IndexError("Index out of range")
+    if slot < 0 or slot > self.num_records:
+      raise IndexError("Writing invalid slot in physical page")
 
-      return int.from_bytes(self.data[slot*8 : (slot+1)*8], byteorder='big')
+    val_to_byte = value.to_bytes(8, byteorder='big')
+    # Insert by bytes
+    i = slot * 8
+    for byte in val_to_byte:
+      self.data[i] = byte
+      i += 1
+    self.num_records += 1
 
+
+  def read(self, slot):
+    if slot > 512 or slot < 0 or slot >= self.num_records: raise IndexError("Index out of range")
+
+    return int.from_bytes(self.data[slot * 8: (slot + 1) * 8], byteorder='big')
