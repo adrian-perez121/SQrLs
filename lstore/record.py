5df8e4e5f47b80be694931ec4f0e9768143017c1
Adrian Perez <appperez@ucdavis.edu>
2025-03-01 14:59:25 -0800

    Clean up record init

--- lstore/record.py
+++ lstore/record.py
@@ -1,9 +1,12 @@
+import lstore.config as config
+
 class Record:
 
-  def __init__(self, indirection, rid, timestamp, schema_encoding, key, columns):
-    self.indirection = indirection
-    self.rid = rid
-    self.timestamp = timestamp
-    self.schema_encoding = schema_encoding
+  def __init__(self, record_data, key):
+    self.entire_record = record_data
+    self.indirection = record_data[config.INDIRECTION_COLUMN]
+    self.rid = record_data[config.RID_COLUMN]
+    self.timestamp = record_data[config.TIMESTAMP_COLUMN]
+    self.schema_encoding = record_data[config.SCHEMA_ENCODING_COLUMN]
     self.key = key
-    self.columns = columns
+    self.columns = record_data[config.NUM_META_COLUMNS:]
